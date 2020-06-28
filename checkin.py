71ecea5c156e5be878bc0132c4c77d74fbae8847
xiaomustudent <62041219+xiaomustudent@users.noreply.github.com>
2020-06-28 19:33:10 +0800

    Update checkin.py

--- checkin.py
+++ checkin.py
@@ -1,11 +1,12 @@
-import requests, time, re, json, base64, argparse
-from urllib import parse
-# server酱开关，填0不开启(默认)，填1只开启cookie失效通知，填2同时开启cookie失效通知和签到成功通知
-sever = '2'
+import requests,json
+# server酱开关，填off不开启(默认)，填on同时开启cookie失效通知和签到成功通知
+sever =  ${{ secrets.sever }}
 # 填写server酱sckey,不开启server酱则不用填
-sckey = 'SCU89402Tf98b7f01ca3394b9ce9aa5e2ed1abbae5e6ca42796bb9'
+sckey =  ${{ secrets.sckey }}
+#'SCU89402Tf98b7f01ca3394b9ce9aa5e2ed1abbae5e6ca42796bb9'
 # 填入glados账号对应cookie
-cookie = '__cfduid=d3459ec306384ca67a65170f8e2a5bd561593049467; _ga=GA1.2.766373509.1593049472; _gid=GA1.2.1338236108.1593049472; koa:sess=eyJ1c2VySWQiOjQxODMwLCJfZXhwaXJlIjoxNjE4OTY5NTI4MzY4LCJfbWF4QWdlIjoyNTkyMDAwMDAwMH0=; koa:sess.sig=6qG8SyMh_5KpSB6LBc9yRviaPvI'
+cookie =  ${{ secrets.cookie }}
+#'__cfduid=d3459ec306384ca67a65170f8e2a5bd561593049467; _ga=GA1.2.766373509.1593049472; _gid=GA1.2.1338236108.1593049472; koa:sess=eyJ1c2VySWQiOjQxODMwLCJfZXhwaXJlIjoxNjE4OTY5NTI4MzY4LCJfbWF4QWdlIjoyNTkyMDAwMDAwMH0=; koa:sess.sig=6qG8SyMh_5KpSB6LBc9yRviaPvI'
 referer = 'https://glados.rocks/console/checkin'
 
 def start():
@@ -20,7 +21,7 @@ def start():
         time = state.json()['data']['leftDays']
         time = time.split('.')[0]
         #print(time)
-        if sever == '2':
+        if sever == 'on':
             requests.get('https://sc.ftqq.com/' + sckey + '.send?text='+mess+'，you have '+time+' days left')
     else:
         requests.get('https://sc.ftqq.com/' + sckey + '.send?text=cookie过期')

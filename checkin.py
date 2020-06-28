4ece5f09e85375d961b7dcca93e949e625b3ba6f
xiaomustudent <62041219+xiaomustudent@users.noreply.github.com>
2020-06-28 21:51:46 +0800

    Update checkin.py

--- checkin.py
+++ checkin.py
@@ -1,8 +1,16 @@
 import requests,json,os
+ # server酱开关，填off不开启(默认)，填on同时开启cookie失效通知和签到成功通知
+    sever =  os.environ["SERVE"]
+# 填写server酱sckey,不开启server酱则不用填
+    sckey =  os.environ["SCKEY"]
+#'SCU89402Tf98b7f01ca3394b9ce9aa5e2ed1abbae5e6ca42796bb9'
+# 填入glados账号对应cookie
+    cookie =  os.environ["COOKIE"]
+#'__cfduid=d3459ec306384ca67a65170f8e2a5bd561593049467; _ga=GA1.2.766373509.1593049472; _gid=GA1.2.1338236108.1593049472; koa:sess=eyJ1c2VySWQiOjQxODMwLCJfZXhwaXJlIjoxNjE4OTY5NTI4MzY4LCJfbWF4QWdlIjoyNTkyMDAwMDAwMH0=; koa:sess.sig=6qG8SyMh_5KpSB6LBc9yRviaPvI'
 
 
-
-def start(sever,sckey,cookie):
+def start():
+    
     url= "https://glados.rocks/api/user/checkin"
     url2= "https://glados.rocks/api/user/status"
     referer = 'https://glados.rocks/console/checkin'
@@ -24,15 +32,6 @@ def main_handler(event, context):
   return start()
 
 if __name__ == '__main__':
-    
- # server酱开关，填off不开启(默认)，填on同时开启cookie失效通知和签到成功通知
-    sever =  os.environ["SERVE"]
-# 填写server酱sckey,不开启server酱则不用填
-    sckey =  os.environ["SCKEY"]
-#'SCU89402Tf98b7f01ca3394b9ce9aa5e2ed1abbae5e6ca42796bb9'
-# 填入glados账号对应cookie
-    cookie =  os.environ["COOKIE"]
-#'__cfduid=d3459ec306384ca67a65170f8e2a5bd561593049467; _ga=GA1.2.766373509.1593049472; _gid=GA1.2.1338236108.1593049472; koa:sess=eyJ1c2VySWQiOjQxODMwLCJfZXhwaXJlIjoxNjE4OTY5NTI4MzY4LCJfbWF4QWdlIjoyNTkyMDAwMDAwMH0=; koa:sess.sig=6qG8SyMh_5KpSB6LBc9yRviaPvI'
-    start(sever,sckey,cookie)
+    start()
 
     
